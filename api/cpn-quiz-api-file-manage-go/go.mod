20f3f3404952a66ffbe7e8e6829828e9ac306904
awesome dev <dev.awesome.th@gmail.com>
2024-03-18 23:06:13 +0700

    update complete authentication

--- api/cpn-quiz-api-file-manage-go/go.mod
+++ api/cpn-quiz-api-file-manage-go/go.mod
@@ -9,7 +9,7 @@ require (
 	github.com/tylerb/graceful v1.2.15
 	golang.org/x/exp v0.0.0-20240314144324-c7f7c6466f7f
 	gopkg.in/go-playground/validator.v9 v9.31.0
-	gorm.io/driver/sqlserver v1.5.3
+	gorm.io/driver/postgres v1.5.7
 	gorm.io/gorm v1.25.7
 )
 
@@ -18,9 +18,10 @@ require (
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
-	github.com/golang-sql/civil v0.0.0-20220223132316-b832511892a9 // indirect
-	github.com/golang-sql/sqlexp v0.1.0 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
+	github.com/jackc/pgx/v5 v5.4.3 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/labstack/gommon v0.4.2 // indirect
@@ -28,7 +29,6 @@ require (
 	github.com/magiconair/properties v1.8.7 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
-	github.com/microsoft/go-mssqldb v1.6.0 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/pelletier/go-toml/v2 v2.1.0 // indirect
 	github.com/sagikazarmark/locafero v0.4.0 // indirect
--- api/cpn-quiz-api-mailer-go/app/main.go
+++ api/cpn-quiz-api-mailer-go/app/main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	// "cpn-quiz-api-mailer-go/database"
+	"cpn-quiz-api-mailer-go/database"
+
 	"cpn-quiz-api-mailer-go/logger"
-	"net/http"
 	"time"
 
 	"fmt"
@@ -24,53 +24,51 @@ import (
 var log = new(logger.PatternLogger).InitLogger("ALL", config.GetString("service.name"), logger.Service, logger.Database)
 
 func init() {
+	//=>Get argument of command.
 	env := os.Args[1]
 	log.Info("", fmt.Sprintf("Server start running on %s environment configuration", env))
 
+	//=>Concat string for get file in config directory.
 	config.SetConfigFile(fmt.Sprintf("config/%s.yml", env))
 	if err := config.ReadInConfig(); err != nil {
+		//=>Cannot load configuration.
 		log.Info("", fmt.Sprintf("Fatal error env config (file): %s", env))
 		panic(err)
 	}
 
+	//=>Load env.yml
 	config.SetConfigFile((".env.yml"))
 	if err := config.MergeInConfig(); err != nil {
 		panic(err)
 	}
 
-	log.Info("", config.GetString("service.port"))
-
-	//=>ต้อง Connected VPN ด้วยถึงจะเรียก LoadConfig ได้
-	// database.LoadConfig()
+	//=>Load db config.
+	database.LoadConfig()
 }
 
 func main() {
-	// db := database.Database{}
-	// db.Log = *log
-	// dbConn := db.GetConnectionDB()
+	db := database.Database{}
+	db.Log = *log
+	dbConn := db.GetConnectionDB()
 
+	//=>new echo context
 	e := echo.New()
+
+	//=>set middleware
 	e.Use(middleware.CORS())
 	e.Use(middleware.Recover())
-	e.Use(middleware.BodyLimit("150M"))
+	e.Use(middleware.BodyLimit("1M"))
 
-	//=>หามี Path สำหรับแสดง Statics Files ให้เปิดใช้งาน
-	// e.Static(config.GetString("service.endpoint")+"/static", "cpm/static")
-
-	//=>First Route
-	eg := e.Group(config.GetString("service.endpoint"))
-	eg.GET("", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World")
-	})
-
-	helloWorldRepository := _helloWorldRepository.NewHelloWorldRepository(nil)
+	//=>domain data-access & business logic
+	helloWorldRepository := _helloWorldRepository.NewHelloWorldRepository(dbConn)
 	helloWorldUseCase := _helloWorldUseCase.NewHelloWorldUseCase(helloWorldRepository, log)
 
+	//=>domain delivery
 	_helloWorldDelivery.NewHelloWorldDelivery(e, helloWorldUseCase, log)
 
+	//=>launch server & port
 	e.Server.Addr = ":" + config.GetString("service.port")
 	if err := graceful.ListenAndServe(e.Server, 5*time.Second); err != nil {
 		panic(err)
 	}
-
 }
