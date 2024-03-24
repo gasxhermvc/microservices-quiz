2709bb26ce9f4631cc97d5da19dd499081a378c1
awesome dev <dev.awesome.th@gmail.com>
2024-03-24 16:21:39 +0700

    fix bug authorization key

--- api/cpn-quiz-api-file-manage-go/middleware/service-authorize/service_authorize.go
+++ api/cpn-quiz-api-file-manage-go/middleware/service-authorize/service_authorize.go
@@ -32,108 +32,110 @@ func (cag *CustomAuthorizeGuard) AuthorizeGuard() echo.MiddlewareFunc {
 			cag.Mutex.Lock()
 			defer cag.Mutex.Unlock()
 
-			clientId := c.Request().Header.Get("x-client-id")
-			authorization := c.Request().Header.Get("x-api-key")
+			token := c.Request().Header.Get("Authorization")
+			if token == "" {
+				clientId := c.Request().Header.Get("x-client-id")
+				authorization := c.Request().Header.Get("x-api-key")
 
-			//=>Check client is not empty
-			if clientId == "" {
-				msg = "Un authorization, required is 'x-client-id'"
-				cag.Log.Error(transId, msg)
-				return c.JSON(http.StatusUnauthorized, domain.Response{
-					TransactionId: transId,
-					Message:       msg,
-					Code:          constant.UnAuthorizationCode,
-				})
-			}
+				//=>Check client is not empty
+				if clientId == "" {
+					msg = "Un authorization, required is 'x-client-id'"
+					cag.Log.Error(transId, msg)
+					return c.JSON(http.StatusUnauthorized, domain.Response{
+						TransactionId: transId,
+						Message:       msg,
+						Code:          constant.UnAuthorizationCode,
+					})
+				}
 
-			//=>Check apiKey is not empty
-			if authorization == "" {
-				msg = "Un authorization, required is 'x-api-key'"
-				cag.Log.Error(transId, msg)
-				return c.JSON(http.StatusUnauthorized, domain.Response{
-					TransactionId: transId,
-					Message:       constant.UnAuthorization,
-					Code:          constant.UnAuthorizationCode,
-					ErrorResponse: domain.ErrorResponse{
-						Error: []string{msg},
-					},
-				})
-			}
+				//=>Check apiKey is not empty
+				if authorization == "" {
+					msg = "Un authorization, required is 'x-api-key'"
+					cag.Log.Error(transId, msg)
+					return c.JSON(http.StatusUnauthorized, domain.Response{
+						TransactionId: transId,
+						Message:       constant.UnAuthorization,
+						Code:          constant.UnAuthorizationCode,
+						ErrorResponse: domain.ErrorResponse{
+							Error: []string{msg},
+						},
+					})
+				}
 
-			//=>Check is required 'Only' on route
-			if cag.Only != "" && !strings.EqualFold(strings.ToLower(clientId), strings.ToLower(cag.Only)) {
-				msg = "Un authorization, Permission denied."
-				cag.Log.Error(transId, msg)
-				return c.JSON(http.StatusUnauthorized, domain.Response{
-					TransactionId: transId,
-					Message:       constant.UnAuthorization,
-					Code:          constant.UnAuthorizationCode,
-					ErrorResponse: domain.ErrorResponse{
-						Error: []string{msg},
-					},
-				})
-			}
+				//=>Check is required 'Only' on route
+				if cag.Only != "" && !strings.EqualFold(strings.ToLower(clientId), strings.ToLower(cag.Only)) {
+					msg = "Un authorization, Permission denied."
+					cag.Log.Error(transId, msg)
+					return c.JSON(http.StatusUnauthorized, domain.Response{
+						TransactionId: transId,
+						Message:       constant.UnAuthorization,
+						Code:          constant.UnAuthorizationCode,
+						ErrorResponse: domain.ErrorResponse{
+							Error: []string{msg},
+						},
+					})
+				}
 
-			apiKey := authorization
-			//=>ตรวจสอบ x-api-key
-			if len(apiKey) < 1 {
-				msg = "Un authorization, please check your ApiKey"
-				cag.Log.Error(transId, msg)
-				return c.JSON(http.StatusUnauthorized, domain.Response{
-					TransactionId: transId,
-					Message:       constant.UnAuthorization,
-					Code:          constant.UnAuthorizationCode,
-					ErrorResponse: domain.ErrorResponse{
-						Error: []string{msg},
-					},
-				})
-			}
+				apiKey := authorization
+				//=>ตรวจสอบ x-api-key
+				if len(apiKey) < 1 {
+					msg = "Un authorization, please check your ApiKey"
+					cag.Log.Error(transId, msg)
+					return c.JSON(http.StatusUnauthorized, domain.Response{
+						TransactionId: transId,
+						Message:       constant.UnAuthorization,
+						Code:          constant.UnAuthorizationCode,
+						ErrorResponse: domain.ErrorResponse{
+							Error: []string{msg},
+						},
+					})
+				}
 
-			//=>Get from global list data
-			secretKey := AuthorizeApiKeyLists
-			match := new(SecretKey)
+				//=>Get from global list data
+				secretKey := AuthorizeApiKeyLists
+				match := new(SecretKey)
 
-			for _, v := range secretKey {
-				if strings.EqualFold(strings.ToLower(v.ClientId), strings.ToLower(clientId)) &&
-					strings.EqualFold(v.ApiKey, apiKey) {
-					match.ClientId = v.ClientId
-					match.ApiKey = v.ApiKey
+				for _, v := range secretKey {
+					if strings.EqualFold(strings.ToLower(v.ClientId), strings.ToLower(clientId)) &&
+						strings.EqualFold(v.ApiKey, apiKey) {
+						match.ClientId = v.ClientId
+						match.ApiKey = v.ApiKey
+					}
+				}
+
+				//=>Check match is empty
+				if match.ClientId == "" || match.ApiKey == "" {
+					msg = "Un authorization, client-id or api-key incorrect."
+					cag.Log.Error(transId, msg)
+					return c.JSON(http.StatusUnauthorized, domain.Response{
+						TransactionId: transId,
+						Message:       msg,
+						Code:          constant.UnAuthorizationCode,
+					})
+				}
+
+				//=>Check api not matching
+				if match.ApiKey != apiKey {
+					msg = "Un authorization, ClientId or ApiKey incorrect."
+					cag.Log.Error(transId, msg)
+					return c.JSON(http.StatusUnauthorized, domain.Response{
+						TransactionId: transId,
+						Message:       msg,
+						Code:          constant.UnAuthorizationCode,
+					})
+				}
+
+				//=>Next or Error
+				if err := next(c); err != nil {
+					msg = err.Error()
+					cag.Log.Error(transId, msg)
+					return c.JSON(http.StatusUnauthorized, domain.Response{
+						TransactionId: transId,
+						Message:       msg,
+						Code:          constant.UnAuthorizationCode,
+					})
 				}
 			}
-
-			//=>Check match is empty
-			if match.ClientId == "" || match.ApiKey == "" {
-				msg = "Un authorization, client-id or api-key incorrect."
-				cag.Log.Error(transId, msg)
-				return c.JSON(http.StatusUnauthorized, domain.Response{
-					TransactionId: transId,
-					Message:       msg,
-					Code:          constant.UnAuthorizationCode,
-				})
-			}
-
-			//=>Check api not matching
-			if match.ApiKey != apiKey {
-				msg = "Un authorization, ClientId or ApiKey incorrect."
-				cag.Log.Error(transId, msg)
-				return c.JSON(http.StatusUnauthorized, domain.Response{
-					TransactionId: transId,
-					Message:       msg,
-					Code:          constant.UnAuthorizationCode,
-				})
-			}
-
-			//=>Next or Error
-			if err := next(c); err != nil {
-				msg = err.Error()
-				cag.Log.Error(transId, msg)
-				return c.JSON(http.StatusUnauthorized, domain.Response{
-					TransactionId: transId,
-					Message:       msg,
-					Code:          constant.UnAuthorizationCode,
-				})
-			}
-
 			return nil
 		}
 	}
